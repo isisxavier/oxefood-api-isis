a013dbfa462df57ad552d0d7aa902f74ee391e27
Isis Xavier <isisxavierti@gmail.com>
2023-05-25 20:22:55 -0300

    editar

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -27,9 +27,25 @@ public class ProdutoService extends GenericService{
     return repository.findAll();
 }
 
-public Produto obterPorID(Long id) {
+    public Produto obterPorID(Long id) {
 
-    return repository.findById(id).get();
-}
+        return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void update(Long id, Produto produtoAlterado) {
+
+    Produto produto = repository.findById(id).get();
+    produto.setCodigo(produtoAlterado.getCodigo());
+    produto.setTitulo(produtoAlterado.getTitulo());
+    produto.setDescricao(produtoAlterado.getDescricao());
+    produto.setValorUnitario(produtoAlterado.getValorUnitario());
+    produto.setTempoEntregaMinimo(produtoAlterado.getTempoEntregaMinimo());
+    produto.setTempoEntregaMaximo(produtoAlterado.getTempoEntregaMaximo());
+        
+    super.preencherCamposAuditoria(produto);
+    repository.save(produto);
+    }
+
     
 }
